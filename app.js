e37d943c511a941a6184bd725ff159b3be63d425
Eduardo aguirre <eduardociep8@gmail.com>
2026-02-09 19:59:31 -0300

    Deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -36,7 +36,7 @@ function verificarChute() {
     }
 }
 
-function gerarNumeroAleatorio() {
+function gerarNumeroAleatorio() {   
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
 
